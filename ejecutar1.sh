30d23535ade26ad09ea154f73ac909d40ddd679a
khmRM <khm691@gmail.com>
2016-01-06 21:19:11 +0100

    Fix hendrix problems

--- ejecutar1.sh
+++ ejecutar1.sh
@@ -2,5 +2,4 @@
 rm -Rf bin
 mkdir bin
 javac -d ./bin/ -cp ./bin/ ./src/es/arcri/sat/exceptions/*.java ./src/es/arcri/sat/*.java
-cd bin
-java es.arcri.sat.Main ../Tests/
+java -cp ./bin/ es.arcri.sat.Main Tests/
